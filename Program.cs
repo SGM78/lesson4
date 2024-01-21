6a4a8894293e1f77d777375c8e00a4c9ca3557a3
Marchenkov Sergei <serguei1978@mail.ru>
2024-01-21 18:04:33 +0300

    программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите целое число или 'q' для завершения:");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "q")
+            {
+                Console.WriteLine("Программа завершена.");
+                break;
+            }
+
+            if (int.TryParse(input, out int number))
+            {
+                                if (IsSumOfDigitsEven(number))
+                {
+                    Console.WriteLine($"Сумма цифр числа {number} четная. Программа завершена.");
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Введите целое число или 'q' для завершения.");
+            }
+        }
+    }
+
+       static bool IsSumOfDigitsEven(int num)
+    {
+        int sum = 0;
+        while (num != 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return sum % 2 == 0;
+    }
+}
